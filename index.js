1047f4ece54ddff6964c96e0c62828eb6d724bbd
Kieran Schwegman <kschwegman@demandjump.com>
2020-06-15 14:12:06 -0400

    Server essentials with routing and configuration altogether

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const sqlc = require("./db");
 const mysql = require("mysql");
+const sqlc = require("./db");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
